82e2b4566bed23e3ba91cb7d35f6827f2e59ad71
Craig Smith <craig_e_smith@cooperators.ca>
2023-01-11 09:29:24 -0500

    updated gitignore

--- sample-nodejs-ms/dist/router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router
-    .get('/status', function (request, response) {
-    response.send({ status: 'Running', timestamp: new Date() });
-});
-exports.default = router;
